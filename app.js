84f9121b9c6a2a412c03f35c32edb0e48f6696fc
Joey Zheng <jzheng16@binghamton.edu>
2017-01-16 18:30:47 -0500

    not even close to done

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-fasvbaivnai
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+/*const http = require('http');
+
+const server = http.createServer();*/
+app.listen(PORT, function(){
+  console.log("Server listening");
+})
+
+app.get('/', function(req,res){
+  console.log("WELCOME MESSAGE");
+  res.send('HELLO');
+})
+
+
