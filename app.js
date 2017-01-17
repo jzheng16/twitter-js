32cbea75da12110d86c335dc074c0658beb3d6a1
Joey Zheng <jzheng16@binghamton.edu>
2017-01-17 11:22:49 -0500

    completed part 5

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 const express = require('express');
 const app = express();
-
 const PORT = 3000;
 
-var ourRandomArray = [];
-/*const http = require('http');
+//Nunjuck render code
+const nunjucks = require('nunjucks');
+app.set('view engine', 'html');        //Sets view engine to html default
+app.engine('html', nunjucks.render);		//Tells our app that we are using nunjuck to render html
+nunjucks.configure('views', {noCache: true});    //tells our app where our templates are
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
 
-const server = http.createServer();*/
+app.all('*', function(req, res, next){
 
-
-
-app.all('*',function(req,res, next){
 	console.log(req.method, 'this is the verb');
 	console.log(req.originalUrl, 'this is the route originalUrl');
-	// console.log(req);
-
 	// console.log(req.params);
 	next();
 })
 
-app.use('/joey/', function(req,res,next){
+app.use('/joey/', function(req, res, next){
 	console.log('this is the middleware ONLY for /joey');
 	next();
 })
@@ -30,13 +35,12 @@ app.post('/joey', function(req, res){
 })
 
 
-
 app.listen(PORT, function(){
-  console.log("Server listening..this is what you get when you open your server");
+  console.log('Server listening..this is what you get when you open your server');
 })
 
-app.get('/', function(req,res){
-  console.log("WELCOME MESSAGE this is what i get when someone requests");
+app.get('/', function(req, res){
+  console.log('WELCOME MESSAGE this is what i get when someone requests');
   res.send('HELLO');
 })
 
@@ -45,9 +49,14 @@ app.get('/joey', function(req, res){
 })
 
 
-
 app.get('/news', function(req, res){
 	console.log('someone requested the /news');
 	res.send('good job, you accessed /news');
 })
 
+
+app.get('/eric', function(req, res){
+
+	res.render( 'index', locals );
+
+})
